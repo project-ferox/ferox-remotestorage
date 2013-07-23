ec19396c8d26bb2d7632dd3ed43356f4ebfbeb4b
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-07-22 20:47:32 -0400

    Clean before deploy

--- ferox.remotestorage.server.standalone/deploy-dev.bat.sh
+++ ferox.remotestorage.server.standalone/deploy-dev.bat.sh
@@ -1,3 +1,8 @@
 mvn package
 
+cd ../felix
+./clean.sh
+
+cd ../ferox.remotestorage.server.standalone
+
 cp -r target/runtime/* ../felix
--- ferox.remotestorage.server.standalone/deploy-remote.bat.sh
+++ ferox.remotestorage.server.standalone/deploy-remote.bat.sh
@@ -1,5 +1,7 @@
 mvn package
 
+ssh tantaman@tantaman.com:/home/tantaman/remotestorage-server clean.sh
+
 scp -r target/runtime/* tantaman@tantaman.com:/home/tantaman/remotestorage-server/
 
 scp target/runtime/configurations/services/ferox.remotestorage.route_initializer.properties.production tantaman@tantaman.com:/home/tantaman/remotestorage-server/configurations/services/ferox.remotestorage.route_initializer.properties
